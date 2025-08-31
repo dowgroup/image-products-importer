09d02a6449f89b65fbc80c06afbd412ca81c44b6
Dow Group <dowgeorges10@gmail.com>
2025-08-31 13:49:06 +0400

    Update __manifest__.py

--- image_products_importer/__manifest__.py
+++ image_products_importer/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Product Image Importer',
+    'name': 'Bulk Image Importer',
     'version': '1.0',
     'summary': 'Easily bulk import product images by matching filenames to product references, names, or barcodes.',
     'category': 'Inventory',
@@ -16,30 +16,21 @@
     'installable': True,
     'application': True,
     'description': """
-The Bulk Image Importer for Odoo makes product image management fast, accurate, and stress-free. 
-No more renaming files or uploading one by one — just drop your images in a folder and let the app do the work.
-
-What Makes It Smart
--------------------
-- **Case-Insensitive Matching**: Works no matter how your file names look.
-  - Blue Chair.jpg, blue-chair.jpg, or Blue_Chair.JPG → all match the product Blue Chair.
-  - CHAIR123.jpg → matches product chair123.
-- Bulk Upload: Import hundreds or thousands of images in one go.
-- Smart Matching: Finds the right product by Internal Reference (SKU), Barcode, or Product Name.
-- Duplicate-Safe: Skips images that are already uploaded.
-- Error-Proof: Files that don’t match any product are skipped automatically.
-- Handles Big Catalogs: Optimized for large imports without slowing down Odoo.
-- Easy Wizard UI: Anyone can use it — no technical skills needed.
-
-Why Choose This Importer?
--------------------------
-It’s built to save you time, prevent mistakes, and handle real-world file names — making it the most reliable way to keep your Odoo catalog updated with images.
-
-Faster. Smarter. Easier.
+Bulk Image Importer module for Odoo allows you to easily import product images
+by matching filenames to product references, names, or barcodes. The app supports
+case insensitivity, ignores dashes and underscores, and processes images in batches 
+for smoother operation.
     """,
     'images': [
-        'static/description/odoo_app_thumbnail.png',  
+        'static/description/odoo_app_thumbnail.png',
+        'static/description/odoo image preview.svg',
+        'static/description/odoo image preview(2).svg',
+        'static/description/odoo image preview(3).svg',
+        'static/description/odoo image preview(4).svg',
+        'static/description/odoo image preview(5).svg',
+        'static/description/odoo image preview(6).svg',
     ],
+    'icon': 'static/description/module_icon.png',
     'price': 100.0,
     'currency': 'USD',
 }
