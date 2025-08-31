99805c966d513a7e32fa82e0dd252a94c1897b3b
Dow Group <dowgeorges10@gmail.com>
2025-08-31 14:06:41 +0400

    Update __manifest__.py

--- image_products_importer/__manifest__.py
+++ image_products_importer/__manifest__.py
@@ -46,7 +46,7 @@ Faster. Smarter. Easier.
         'static/description/odoo image preview(5).svg',
         'static/description/odoo image preview(6).svg',
     ],
-    'icon': 'static/description/module_icon.png',
+    'icon': 'static/description/icon.png',
     'price': 100.0,
     'currency': 'USD',
 }
